11e8985d1d68e9e67d919a63c7ff3ebdd627b242
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-10-27 19:51:10 -0700

    WIP building out the GCE api

--- builder/googlecompute/builder.go
+++ builder/googlecompute/builder.go
@@ -2,9 +2,58 @@
 // builds images for Google Compute Engine.
 package googlecompute
 
-// The unique ID for this builder
-const BuilderId = "googlecompute"
+import (
+	"errors"
+	"fmt"
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/common"
+	"github.com/mitchellh/packer/packer"
+	"log"
+	"os"
+	"time"
+)
 
-func Run() {
-	// Nothing yet.	
+// The unique ID for this builder
+const BuilderId = "kelseyhightower.googlecompute"
+
+type Builder struct {
+	config config
+	runner multistep.Runner
+}
+
+type config struct {
+	common.PackerConfig `mapstructure:",squash"`
+	AuthURI             string            `mapstructure:"auth_uri"`
+	ClientEmail         string            `mapstructure:"client_email"`
+	ClientId            string            `mapstructure:"client_id"`
+	ImageName           string            `mapstructure:"image_name"`
+	ImageDescription    string            `mapstructure:"image_description"`
+	MachineType         string            `mapstructure:"machine_type"`
+	Metadata            map[string]string `mapstructure:"metadata"`
+	Network             string            `mapstructure:"network"`
+	PrivateKeyPath      string            `mapstructure:"private_key_path"`
+	ProjectId           string            `mapstructure:"project_id"`
+	SourceImage         string            `mapstructure:"source_image"`
+	SSHUsername         string            `mapstructure:"ssh_username"`
+	SSHPort             uint              `mapstructure:"ssh_port"`
+	RawSSHTimeout       string            `mapstructure:"ssh_timeout"`
+	RawStateTimeout     string            `mapstructure:"state_timeout"`
+	Tags                []string          `mapstructure:"tags"`
+	TokenURI            string            `mapstructure:"token_uri"`
+	Zone                string            `mapstructure:"zone"`
+	sshTimeout          time.Duration
+	stateTimeout        time.Duration
+	tpl                 *packer.ConfigTemplate
+}
+
+func (b *Builder) Prepare(raws ...interface{}) error {
+	// Nothing yet.
+}
+
+func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
+	// Nothing yet.
+}
+
+func (b *Builder) Cancel() {
+	// Nothing yet.
 }
