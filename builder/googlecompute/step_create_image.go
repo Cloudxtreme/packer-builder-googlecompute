733f9ab1f784b52f2253972da345e42480079db8
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-11-16 21:11:21 -0800

    Code review and clean up

--- builder/googlecompute/step_create_image.go
+++ builder/googlecompute/step_create_image.go
@@ -62,5 +62,5 @@ func (s *stepCreateImage) Run(state multistep.StateBag) multistep.StepAction {
 }
 
 func (s *stepCreateImage) Cleanup(state multistep.StateBag) {
-	return
+	// Nothing to cleanup.
 }
